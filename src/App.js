d6ac93576d83fd0c783b01b7931240bcd7c42e71
ssal-man <ssalmanyo9@gmail.com>
2020-07-03 08:03:42 +0530

    particle.js added idk why

--- src/App.js
+++ src/App.js
@@ -10,8 +10,119 @@ import { createUserProfile } from './firebase/firebase.utils';
 import { connect} from 'react-redux';
 import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from './redux/user/user.selector';
-import  CheckOut  from './page/checkout/checkout.component';
+import CheckOut from './page/checkout/checkout.component';
+import Particles from 'react-particles-js';
 
+const ParticleOption = {
+  "particles": {
+    "number": {
+      "value": 213,
+      "density": {
+        "enable": false,
+        "value_area": 1893.9543399174545
+      }
+    },
+    "color": {
+      "value": "#cacaca"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.0,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 10,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 500,
+      "color": "#ffffff",
+      "opacity": 0.2,
+      "width": 2
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "bottom",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 0.5
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 4,
+        "duration": 0.3,
+        "opacity": 1,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": false
+}
 class App extends React.Component {
 
   unsubscribeFromAuth=null; 
@@ -42,6 +153,8 @@ class App extends React.Component {
   render(){
     return (
       <div>
+        <Particles className="particles"
+          params={ParticleOption} />
       <Header/>
       <hr/>
       <Switch>
@@ -49,7 +162,7 @@ class App extends React.Component {
         <Route exact path='/shop' component={Shoppage}/>
         <Route exact path='/signin' render={()=>this.props.currentUser?<Redirect to='/'/>:<SigninSignup/>}/>
         <Route exact path='/checkout' component={CheckOut}/>
-      </Switch>
+          </Switch>
     </div>
   );
 }
